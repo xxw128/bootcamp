e5859504ba8444d8ff32d4f20f2bd83ece485ef4
xiawu <xiawu@caltech.edu>
2017-06-22 13:02:55 -0700

    Added the draw_bs_reps function

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -22,3 +22,12 @@ def ecdf(data):
     y = np.arange(1, len(data)+1)/len(data)
 
     return x, y
+
+def bs_replicate(data, func=np.mean):
+    """Compute a bootstrap replicate for data, doing the 'func' function."""
+    bs_sample = np.random.choice(data, replace=True, size=len(data))
+    return func(bs_sample)
+
+def draw_bs_reps(data, func=np.mean, size=10000):
+    """Draw bootstrap replicates from 1d data."""
+    return np.array([bs_replicate(data, func=func) for _ in range(size)])
